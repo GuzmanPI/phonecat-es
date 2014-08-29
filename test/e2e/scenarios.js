503a992626840aae0c1dd96bb85a4c5cbf2e3330
Israel Guzman <israel.guzman@xpertys.com.mx>
2014-08-29 17:28:22 -0500

    Agrega paso 3 de la aplicación
    - Agrega una caja de búsqueda para demostrar como:
      - el enlace de datos (data-binding) funciona en cajas de texto.
      - usar el filtro `filter`.
      - `ngRepeat` automáticamente encoge y crece el número de teléfonos en
    la vista.
    - Y una prueba de end-to-end para:
      - mostrar como se escriben las pruebas de end-to-end y como correrlas
    con Protractor.
      - provear que la caja de búsqueda y el repetidor están conectados
    entre si correctamente.

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -8,4 +8,18 @@ describe('my app', function() {
     browser.get('app/index.html');
   });
 
+  it('debe filtrar la lista de teléfonos mientras el usuario escribe dentro de la caja de busqueda', function() {
+
+    var listaTelefonos = element.all(by.repeater('telefono in telefonos'));
+    var query = element(by.model('query'));
+
+    expect(listaTelefonos.count()).toBe(3);
+
+    query.sendKeys('nexus');
+    expect(listaTelefonos.count()).toBe(1);
+
+    query.clear();
+    query.sendKeys('motorola');
+    expect(listaTelefonos.count()).toBe(2);
+  });
 });
